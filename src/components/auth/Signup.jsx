ee3d6455d8aa98c9cf1e5c209135233e8129a691
gopalbasak1 <gopalbasak1223@gmail.com>
2024-08-01 12:36:20 +0600

    completed login and signup page and connect to the mongodb data base

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,11 +1,168 @@
-
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "../shared/Navbar";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { RadioGroup } from "../ui/radio-group";
+import { useState } from "react";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
 
 const Signup = () => {
-    return (
-        <div>
-            signup
-        </div>
-    );
+
+    const [input, setInput] = useState({
+        fullName:"",
+        email:"",
+        phoneNumber:"",
+        password:"",
+        role:"",
+        file:""
+    });
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {loading} = useSelector(store=>store.auth)
+
+    const changeEventHandler = (e) =>{
+        setInput({...input, [e.target.name]:e.target.value});
+    };
+
+    const changeFileHandler = (e) =>{
+        setInput({...input, file:e.target.files?.[0]});
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("fullName", input.fullName);
+        formData.append("email", input.email);
+        formData.append("phoneNumber", input.phoneNumber);
+        formData.append("password", input.password);
+        formData.append("role", input.role);
+        if(input.file){
+            formData.append("file", input.file);
+        }
+        try {
+          dispatch(setLoading(true))
+            const res = await axios.post(`${USER_API_END_POINT}/register`, formData,{
+                headers:{
+                    "Content-Type": "multipart/form-data"
+                },
+                withCredentials:true,
+            });
+            if(res.data.success){
+                navigate("/login");
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            if (error.response) {
+                toast.error(error.response.data.message);
+              } else {
+                toast.error("Network error, please try again later.");
+              }
+        } finally{
+          dispatch(setLoading(false))
+        }
+
+    }
+
+  return (
+    <div>
+      <Navbar />
+
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md my-10 p-4"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input type="text"
+             value={input.fullName} name="fullName"
+             onChange={changeEventHandler}
+             placeholder="Name" />
+          </div>
+
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input type="email" 
+            value={input.email}
+            name="email"
+            onChange={changeEventHandler}
+            placeholder="Type your email" />
+          </div>
+
+          <div className="my-2">
+            <Label>Phone Number</Label>
+            <Input type="text" 
+            value={input.phoneNumber}
+            name="phoneNumber"
+            onChange={changeEventHandler}
+            placeholder="Type your phone number" />
+          </div>
+
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input type="password"
+             value={input.password}
+             name="password"
+             onChange={changeEventHandler}
+             />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                type="radio"
+                name="role"
+                value="student"
+                checked={input.role === 'student'}
+                onChange={changeEventHandler}
+                className="cursor-pointer"
+                />
+                <Label htmlFor="option-one">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+              <Input
+                type="radio"
+                name="role"
+                value="recruiter"
+                checked={input.role === 'recruiter'}
+                onChange={changeEventHandler}
+                className="cursor-pointer"
+                />
+                <Label htmlFor="option-two">Recruiter</Label>
+              </div>
+            </RadioGroup>
+            <div className="flex items-center gap-2">
+            <Label>Profile</Label>
+            <Input
+            accept="image/*"
+            type="file"
+            onChange={changeFileHandler}
+            className="cursor-pointer"
+            />
+            </div>
+          </div>
+
+          {
+            loading ? <Button className="w-full my-4"><Loader2 className="mr-2 h-4 -w-4 animate-spin"/> Please wait</Button>
+            :
+            <Button type="submit" className="w-full my-4">Signup</Button>
+          }
+
+          <span className="text-sm">Already have an account? <Link to="/login" className="text-blue-500 underline hover:text-blue-700 font-medium">Login</Link></span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
           {!user ? (
             <div className="flex items-center gap-2">
 
-             <Link to="/login"><Button className="bg-[#6A38C2] hover:bg-[#5b32a1]">Login</Button></Link>
+             <Link to="/login"><Button variant="outline">Login</Button></Link>
 
              <Link to="/signup"><Button className="bg-[#6A38C2] hover:bg-[#5b32a1]">Signup</Button></Link>
               
