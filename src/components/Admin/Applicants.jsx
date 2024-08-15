e6f2f5225f8812d98b3fbd4dcdd979c47aa1e20c
gopalbasak1 <gopalbasak1223@gmail.com>
2024-08-15 21:38:08 +0600

    applicants page navbar add

--- src/components/Admin/Applicants.jsx
+++ src/components/Admin/Applicants.jsx
@@ -1,9 +1,13 @@
+import Navbar from "../shared/Navbar";
 
 
 const Applicants = () => {
     return (
         <div>
-            
+            <Navbar/>
+            <div className="max-w-7xl mx-auto">
+                <h1>Applicants(3)</h1>
+            </div>
         </div>
     );
 };
